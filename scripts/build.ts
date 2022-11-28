e0fd63a64899c760d014ba6624ff24050f2d7f33
Leon Si <leon@leondreamed.com>
2022-11-28 14:27:42 -0500

    fix: declarations

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,11 @@
-import { execaCommandSync } from 'execa'
+import { execaCommand, execaCommandSync } from 'execa'
 import { chProjectDir, copyPackageFiles, rmDist } from 'lionconfig'
 
 chProjectDir(import.meta.url)
 rmDist()
-execaCommandSync('tsc')
+await Promise.all([
+	execaCommand('tsc'),
+	execaCommand('tsc --emitDeclarationOnly --declarationDir dist'),
+])
 execaCommandSync('tsc-alias')
 await copyPackageFiles({ commonjs: true })
