18bc6bdbd3aaf18723f3a0afb55681a222859365
Leon Si <leon@leondreamed.com>
2023-02-15 15:44:13 -0500

    chore: no chaneg

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+export type {
+	Route,
+	RouteResponse,
+	RouteResponseFromData,
+} from './types/route.js'
 export { getBody } from '~/utils/body.js'
 export { createRouteBuilder } from '~/utils/builder.js'
 export { getHeaders } from '~/utils/headers.js'
+export { getPathParams } from '~/utils/path-params.js'
 export { redirect } from '~/utils/redirect.js'
 export { getSearchParams } from '~/utils/search-params.js'
-export { getPathParams } from '~/utils/path-params.js'
-export type { Route, RouteResponse, RouteResponseFromData } from './types/route.js'
